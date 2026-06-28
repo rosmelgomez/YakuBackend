4500479e5199ed95f1af5e9e63fb8c5e259b2bae
Rosmel Gomez Benancio <benancio.del1@gmail.com>
2026-06-27 23:20:47 -0500

    modificaciones

--- .build/esp32-riego-latest/core/ColorFormat.c.d
+++
@@ -1,3 +0,0 @@
-C:\Archivos\Code\Yaku-Backend\.build\esp32-riego-latest\core\ColorFormat.c.o: \
- C:\Users\benan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.10\cores\esp32\ColorFormat.c \
- C:\Users\benan\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.10\cores\esp32\ColorFormat.h
