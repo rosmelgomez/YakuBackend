66c62cd8f1edff6d05737d028f4d55770e809bb2
Rosmel Gomez Benancio <benancio.del1@gmail.com>
2026-06-29 17:53:10 -0500

    modificaciones

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#pragma once
+constexpr const char* ssid = "HGB_2,4GHz";
+constexpr const char* password = "@Hgb153427986@";
+constexpr const char* mqtt_user = "hivemq.webclient.1778630712813";
+constexpr const char* mqtt_password = "pVA$d1KU,>R7gM30b@vo";
