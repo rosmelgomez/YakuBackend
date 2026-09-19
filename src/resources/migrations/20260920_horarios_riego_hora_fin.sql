5e4fabe8a39257843fb56f11a6d973209cb82b04
Rosmel Gomez Benancio <benancio.del1@gmail.com>
2026-09-19 09:08:41 -0500

    modificaciones

--- src/resources/migrations/20260920_horarios_riego_hora_fin.sql
+++ src/resources/migrations/20260920_horarios_riego_hora_fin.sql
@@ -4,8 +4,10 @@
 ALTER TABLE horarios_riego ADD COLUMN IF NOT EXISTS hora_fin TIME;
 
 -- Backfill de filas existentes (si las hubiera) sumando la duracion ya guardada.
+-- Nota: no se agrega NOT NULL aqui porque los horarios "siempre_activo" (ver
+-- migracion 20260921) no tienen hora_inicio/hora_fin/duracion_segundos.
 UPDATE horarios_riego
 SET hora_fin = (hora_inicio + (duracion_segundos || ' seconds')::interval)::time
-WHERE hora_fin IS NULL;
-
-ALTER TABLE horarios_riego ALTER COLUMN hora_fin SET NOT NULL;
+WHERE hora_fin IS NULL
+  AND hora_inicio IS NOT NULL
+  AND duracion_segundos IS NOT NULL;
